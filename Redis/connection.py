bff2701dbf9801fae601f2b6d75fe097162c79dd
Lezeu Raul <lezeu.raul@yahoo.com>
2025-11-02 21:44:14 +0200

    git clear cached

--- Redis/connection.py
+++
@@ -1,9 +0,0 @@
-import certifi
-import os
-import redis
-
-HOST = 'LucianDB.redis.cache.windows.net'
-PORT = 6380
-PASSWORD = os.environ.get('REDIS_PASSWORD')
-
-r = redis.StrictRedis(host=HOST, port=PORT, password=PASSWORD, ssl=True, ssl_ca_certs=certifi.where())
